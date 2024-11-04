632b02c3b1d76774d99db9ccb9bd9b1f750b9cc3
Pedro Marinho <pedro.marinho238@gmail.com>
2024-11-03 21:49:45 -0400

    snap

--- lib/app/core/utils/process_custom.dart
+++ lib/app/core/utils/process_custom.dart
@@ -1,16 +1,28 @@
 import 'dart:io';
 
 import 'package:linux_game_tweaks/app/core/utils/flatpak_sandbox.dart';
+import 'package:linux_game_tweaks/app/core/utils/snap_sandbox.dart';
+import 'package:ubuntu_logger/ubuntu_logger.dart';
+
+final log = Logger('Process Custom');
 
 Future<ProcessResult> runCommand(
   String command,
   List<String> args, {
   Map<String, String>? environment,
 }) async {
+
   if (isRunningInFlatpak) {
+    log.info('Running in Flatpak');
     args.insert(0, command);
     args.insert(0, "--host");
     command = 'flatpak-spawn';
+    log.info('Command: $command');
+  } else if (isRunningInSnap) {
+    log.info('Running in Snap');
+    args.insert(0, command);
+    command = 'snap-run';
+    log.info('Command: $command');
   }
   return Process.run(command, args, environment: environment);
 }
@@ -21,9 +33,16 @@ Future<Process> startCommand(
   Map<String, String>? environment,
 }) async {
   if (isRunningInFlatpak) {
+    log.info('Running in Flatpak');
     args.insert(0, command);
     args.insert(0, "--host");
     command = 'flatpak-spawn';
+    log.info('Command: $command');
+  } else if (isRunningInSnap) {
+    log.info('Running in Snap');
+    args.insert(0, command);
+    command = 'snap-run';
+    log.info('Command: $command');
   }
   return Process.start(command, args, environment: environment);
 }
